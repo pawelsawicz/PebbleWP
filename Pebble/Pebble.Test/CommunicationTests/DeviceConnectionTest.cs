72a696211bb95d399c45f4b6240c8f9807a77bab
pawelsawicz <pawel@sawicz.eu>
2013-12-27 19:57:14 +0100

    add blank test methods to Test project

---
+++ Pebble/Pebble.Test/CommunicationTests/DeviceConnectionTest.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pebble.Test.CommunicationTests
+{
+    [TestClass]
+    public class DeviceConnectionTest
+    {
+        [TestInitialize]
+        public void SetUp()
+        {
+
+        }
+
+        [TestMethod]
+        public void connect_to_device()
+        {          
+
+
+
+        }
+    }
+}
